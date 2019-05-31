958c889c88d961f2a64d8e7db5735bd1e171c478
BryceEvans <evans.bryce@gmail.com>
2019-05-30 22:04:57 -0600

    rock paper scissors solved although run time is awful,

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,12 +3,29 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+    allPossibilities = []
+
+    options = [['rock'], ['paper'], ['scissors']]
+
+    if n == 0:
+        return [[]]
+    else:
+        prevPossibilities = rock_paper_scissors(n-1)
+        for poss in prevPossibilities:
+            for o in options:
+                allPossibilities.append(poss + o)
+        
+        return allPossibilities
+
+result = rock_paper_scissors(14)
+
+for r in result:
+    print(r)
 
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_plays = int(sys.argv[1])
+#     print(rock_paper_scissors(num_plays))
+#   else:
+#     print('Usage: rps.py [num_plays]')
