2db8a088bfbc53bb386e1ea574a9e23c532137f6
BryceEvans <evans.bryce@gmail.com>
2019-05-30 19:43:20 -0600

    improved eating cookies function as per example

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -15,6 +15,7 @@ import sys
 #   else:
 #     print('Usage: eating_cookies.py [num_cookies]')
 
+# non-efficient way. Runtime is O(2^n) (not good!)
 def eating_cookies(n):
     if n < 0:
         return 0
@@ -24,3 +25,23 @@ def eating_cookies(n):
         return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 print(eating_cookies(3))
+
+def improved_eating_cookies(n, ways):
+    # base case, valid way
+    if n < 0:
+        return 0
+    # base case, invalid way
+    elif n == 0:
+        return 1
+    # result found, return without recalculating!
+    elif n in ways:
+        return ways[n]
+    # result not found, had to calculate
+    else:
+        result = improved_eating_cookies(n-1, ways) + improved_eating_cookies(n-2, ways) + improved_eating_cookies(n-3, ways)
+        ways[n] = result
+        return result
+
+for i in range(500):
+    print("Ways to eat " + str(i) + " cookies: " + str(improved_eating_cookies(i, {})))
+
